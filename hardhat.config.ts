656dd7335cee5c479cb48082392b55b1840b76aa
benjamin852 <weinbergben33@gmail.com>
2023-12-14 16:34:28 -0500

    able to interact with contract instance

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,24 +1,23 @@
-import dotenv from "dotenv";
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
-import chains from "./chains.json";
+import dotenv from 'dotenv'
+import { HardhatUserConfig } from 'hardhat/config'
+import chains from './chains.json'
 
-dotenv.config();
+dotenv.config()
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.20",
+  solidity: '0.8.20',
   networks: {
     polygon: {
       url: chains[0].rpc,
       accounts: [`0x${process.env.PRIVATE_KEY}`],
-      network_id: 80001,
+      chainId: 80001,
     },
     fantom: {
       url: chains[1].rpc,
       accounts: [`0x${process.env.PRIVATE_KEY}`],
-      network_id: 4002,
+      chainId: 4002,
     },
   },
-};
+}
 
-export default config;
+export default config
--- test/DexBonus.test.ts
+++ test/DexBonus.test.ts
@@ -1,79 +1,112 @@
 import 'dotenv/config';
-import { start } from './utils/Start'
-import { getWallet } from '../utils/getWallet';
-import { getEVMChains } from './utils/GetChains';
-import fs from 'fs-extra';
 import path from 'path';
+import { ContractFactory, utils, BigNumber, Contract } from 'ethers'
 import {
-    deployContract,
-    destroyExported,
-    createNetwork,
-    relay
+  deployContract,
+  destroyExported,
+  createNetwork,
+  relay
 } from '@axelar-network/axelar-local-dev'
 import chains from '../chains.json'
-
-
+import DexBonus from '../artifacts/contracts/DexBonus.sol/DexBonus.json';
 
 describe('Check Examples Execution', function () {
-    before(async () => {
-        // Initialize an Polygon network
-        const polygon = await createNetwork({
-            name: "Polygon",
-        });
-
-        // Deploy USDC token on the polygonereum network
-        await polygon.deployToken("USDC", "aUSDC", 6, BigInt(100_000e6));
-
-        // Initialize an fantom network
-        const fantom = await createNetwork({
-            name: "Fantom",
-        });
-
-        // Deploy USDC token on the fantom network
-        await fantom.deployToken("USDC", "aUSDC", 6, BigInt(100_000e6));
-
-        // Extract user wallets for both polygonereum and fantom networks
-        const [polygonUserWallet] = polygon.userWallets;
-        const [fantomUserWallet] = fantom.userWallets;
-
-        // Mint tokens on the source chain (polygonereum)
-        await polygon.giveToken(polygonUserWallet.address, "aUSDC", BigInt(100e6));
-
-        // Get the token contracts for both polygonereum and fantom networks
-        const usdcpolygonContract = await polygon.getTokenContract("aUSDC");
-        const usdcfantomContract = await fantom.getTokenContract("aUSDC");
-
-        // Approve the gateway to use tokens on the source chain (polygonereum)
-        const polygonApproveTx = await usdcpolygonContract
-            .connect(polygonUserWallet)
-            .approve(polygon.gateway.address, 100e6);
-        await polygonApproveTx.wait();
-
-        // Request the polygonereum gateway to send tokens to the fantom network
-        const polygonGatewayTx = await polygon.gateway
-            .connect(polygonUserWallet)
-            .sendToken(fantom.name, fantomUserWallet.address, "aUSDC", 100e6);
-        await polygonGatewayTx.wait();
-
-        // Relay the transactions
-        await relay();
-
-        // Log the token balances
-        console.log(
-            (await usdcpolygonContract.balanceOf(polygonUserWallet.address)) / 1e6,
-            "aUSDC in polygonereum wallet"
-        );
-        console.log(
-            (await usdcfantomContract.balanceOf(fantomUserWallet.address)) / 1e6,
-            "aUSDC in fantom wallet"
-        );
-
-    })
-    describe('EVM Examples', function () {
-        it('execute example', async function () {
-            console.log('hello')
-            //execute on local chains
-            // await executeEVMExample('local', chains, [], wallet, example);
-        });
+  before(async () => {
+    // Initialize an Polygon network
+    const polygon = await createNetwork({
+      name: "Polygon",
     });
+
+    // Initialize an fantom network
+    const fantom = await createNetwork({
+      name: "Fantom",
+    });
+
+    // Deploy USDC token on the polygon network
+    await polygon.deployToken("USDC", "aUSDC", 6, BigInt(100_000e6));
+
+    // Deploy USDC token on the fantom network
+    await fantom.deployToken("USDC", "aUSDC", 6, BigInt(100_000e6));
+
+    // Extract user wallets for both networks
+    const [polygonUserWallet] = polygon.userWallets;
+    const [fantomUserWallet] = fantom.userWallets;
+
+    // Mint tokens on src chain (polygon)
+    await polygon.giveToken(polygonUserWallet.address, "aUSDC", BigInt(100e6));
+
+    // Get token contracts for both chains
+    const aUsdcPolygonContract = await polygon.getTokenContract("aUSDC");
+    const aUsdcFantomContract = await fantom.getTokenContract("aUSDC");
+
+    // Log balances before transfer
+    console.log(
+      (await aUsdcPolygonContract.balanceOf(polygonUserWallet.address)) / 1e6,
+      "aUSDC in polygon wallet"
+    );
+
+    // Log balances before transfer
+    console.log(
+      (await aUsdcFantomContract.balanceOf(fantomUserWallet.address)) / 1e6,
+      "aUSDC in fantom wallet"
+    );
+
+    // Approve gateway to use tokens on src chain
+    const polygonApproveTx = await aUsdcPolygonContract
+      .connect(polygonUserWallet)
+      .approve(polygon.gateway.address, 100e6);
+    await polygonApproveTx.wait();
+
+    // Request src gateway to send tokens to dest chain
+    const polygonGatewayTx = await polygon.gateway
+      .connect(polygonUserWallet)
+      .sendToken(fantom.name, fantomUserWallet.address, "aUSDC", 100e6);
+    await polygonGatewayTx.wait();
+
+    // Relay the transactions
+    await relay();
+
+    // Log balances after transfer
+    console.log(
+      (await aUsdcPolygonContract.balanceOf(polygonUserWallet.address)) / 1e6,
+      "aUSDC in polygon wallet"
+    );
+    // Log balances after transfer
+    console.log(
+      (await aUsdcFantomContract.balanceOf(fantomUserWallet.address)) / 1e6,
+      "aUSDC in fantom wallet"
+    );
+
+    const createThreeDeployer = await polygon.deployCreate3Deployer()
+
+
+    const creationCode = utils.solidityPack(['bytes', 'bytes'], [DexBonus.bytecode, utils.defaultAbiCoder.encode(['address', 'address', 'address'], [aUsdcPolygonContract.address, polygon.gateway.address, polygon.gasService.address])])
+    const salt = utils.hexZeroPad(BigNumber.from(101), 32);
+
+    const testAddr = await createThreeDeployer.connect(polygonUserWallet).deploy(creationCode, salt)
+    const txReceipt = await testAddr.wait()
+    const deployedAddr = txReceipt.events[0].args[2]
+    console.log(deployedAddr, 'the deployer addr')
+    const dex = new Contract(deployedAddr, DexBonus.abi, polygonUserWallet);
+    console.log(await dex.gasService(), 'the dex')
+
+
+
+
+    //deploy polygon
+    // await deployContract(polygonUserWallet, DexBonus.abi, [aUsdcPolygonContract.address, polygon.gateway.address, polygon.gasService.address])
+    // const dexBonusFantom = await deployContract(polygonUserWallet, DexBonus.abi, [aUsdcFantomContract.address, fantom.gateway.address, fantom.gasService.address])
+
+    // console.log('polygon dexBonus:', dexBonusPolygon.address)
+    // console.log('fantom dexBonus:', dexBonusFantom.address)
+
+  })
+  describe('EVM Examples', function () {
+    it('execute example', async function () {
+      console.log('hello')
+      //execute on local chains
+      // await executeEVMExample('local', chains, [], wallet, example);
+    });
+  });
 });
+
